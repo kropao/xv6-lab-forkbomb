d2fe0cc5f8830cdda5b36279f729774306b46983
kropao <kenzionciang@gmail.com>
2025-11-17 02:40:22 +0800

    a

---
+++ user/4_1.sh
@@ -0,0 +1,13 @@
+echo hello
+jobs
+sleep 10 &
+sleep 2 &
+jobs
+sleep 5
+jobs
+sleep 10
+jobs
+echo Im awake
+sleep 6 &
+jobs
+echo done
---
+++ user/4_2.sh
@@ -0,0 +1,14 @@
+jobs
+jobs
+echo hello
+sleep 5 &
+jobs
+sleep 10 &
+jobs
+sleep 1
+jobs
+sleep 6
+jobs
+sleep 5
+jobs
+echo done
